1ed552bf6617456ab96e182a8ba8f9de478eafd5
adityavyas01 <adityavyas670@gmail.com>
2025-05-27 22:45:01 +0530

    .

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCOiX8x_adK6F0_HGl2qw0z2eutUae10Pk",
-    authDomain: "goalhft.firebaseapp.com",
-    projectId: "goalhft",
-    storageBucket: "goalhft.firebasestorage.app",
-    messagingSenderId: "958598471963",
-    appId: "1:958598471963:web:19bbd875fd9ceb0d90b3be",
-    measurementId: "G-70EFVJLH1H"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
